d1cbbd4fdb4b2454af8afd5bd0e38ed783febf39
sumayya2358 <sumayyanazeer1994@gmail.com>
2022-01-19 22:49:24 +0900

    a hello world commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+
+def function_name(name):
+    print(f'hi, {name}')
+    if name == "Adeeb":
+        print("Adeeb found")
+
+
+if __name__ == '__main__':
+    function_name('Adeeb')
