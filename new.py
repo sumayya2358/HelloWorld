7a4c363e92d7eb272b46a5b26baff2af84aad80b
sumayya2358 <sumayyanazeer1994@gmail.com>
2022-01-21 13:57:41 +0900

    if applied weight converter will be added to the code

---
+++ new.py
@@ -0,0 +1,12 @@
+temperature = 35
+
+if temperature > 30:
+    print("it's a hot day")
+    print("Drink plenty of water")
+elif temperature > 20:
+    print("it's a nice day")
+elif temperature > 10:
+    print("it's a bit cold")
+else:
+    print("it's cold")
+print("Done")
