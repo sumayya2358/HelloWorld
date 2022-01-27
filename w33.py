e44a0aa8f18c17548788db57da7cd504ec965794
sumayya2358 <sumayyanazeer1994@gmail.com>
2022-01-27 14:36:12 +0900

    bool

---
+++ w33.py
@@ -0,0 +1,15 @@
+txt = "we are \n\rvikings\n\r form the north"
+print(txt)
+print(bool("abc"))
+a = 200
+b = 33
+if b>a:
+    print("b is greater than a")
+else:
+    pass
+print("b is not greater than a" )
+
+m = 500
+print(isinstance(m,int))
+j = list(("nan", "pani"))
+print(j)
