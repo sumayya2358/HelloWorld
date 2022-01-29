3a92af7315696978b30504f700ee552e010c9db4
sumayya2358 <sumayyanazeer1994@gmail.com>
2022-01-29 23:40:59 +0900

    function

---
+++ functions.py
@@ -0,0 +1,18 @@
+
+def do_some_action(sumayya, adeeb):
+    love = sumayya + adeeb
+    return love;
+
+
+sasi = do_some_action(1,2)
+print(sasi)
+
+def substract(input_1, input_2):
+    output = input_1 - input_2
+    return output
+
+numbers = substract(45,34)
+print(numbers)
+
+
+
