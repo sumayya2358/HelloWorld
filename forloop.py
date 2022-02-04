3ce5e095f34664e5923f14d46633696492114e42
sumayya2358 <sumayyanazeer1994@gmail.com>
2022-02-05 02:47:44 +0900

    forloop

---
+++ forloop.py
@@ -0,0 +1,11 @@
+#for i in range(5):
+   # for j in range(5):
+       # for w in range(3):
+         #   print(i,j,w)
+
+
+lst = [[1,2],[3,4]]
+for i in range(len(lst)):
+    int_list = lst[i]
+    for j in range(len(int_list)):
+        print(int_list[j])
