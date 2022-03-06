f6854868a14406ef0179c402faf173d6ad646585
sumayya2358 <sumayyanazeer1994@gmail.com>
2022-03-06 22:04:35 +0900

    program error rectified

--- average_age_salary_class.py
+++ average_age_salary_class.py
@@ -13,28 +13,28 @@ class Employee:
 
     @classmethod
     def calculate_average(cls, age, salary):
-        cls.number_of_employees()
+        cls.count_no_of_employees()
         cls.sum_of_age(age)
         cls.sum_of_salary(salary)
-        cls.average_age()
-        cls.average_salary()
+        cls.add_average_age()
+        cls.add_average_salary()
 
     @classmethod
-    def number_of_employees(cls):
+    def count_no_of_employees(cls):
         cls.number_of_employees += 1
 
     @classmethod
     def sum_of_age(cls, age):
-        cls.sum_of_age += age
+        cls.sum_age += age
 
     @classmethod
     def sum_of_salary(cls, salary):
-        cls.sum_of_salary += salary
+        cls.sum_salary += salary
 
     @classmethod
-    def average_age(cls):
-        cls.average_age = cls.sum_of_age / cls.number_of_employees
+    def add_average_age(cls):
+        cls.average_age = cls.sum_age / cls.number_of_employees
 
     @classmethod
-    def average_salary(cls):
-        cls.average_salary = cls.sum_of_salary / cls.number_of_employees
+    def add_average_salary(cls):
+        cls.average_salary = cls.sum_salary / cls.number_of_employees
