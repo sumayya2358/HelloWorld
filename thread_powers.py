913ae04e0f9fbfb1d49b23def93038781d5760f7
sumayya2358 <sumayyanazeer1994@gmail.com>
2022-06-29 04:27:28 +0900

    power of two thread

--- thread_powers.py
+++ thread_powers.py
@@ -1,13 +1,9 @@
+# Copyright © 2022 AlgoExpert LLC. All rights reserved.
+
 import threading
 
 RANGE_START = 0
 RANGE_END = 1000
-
-"""
-At the end of the program, this variable needs to contain all of the powers
-of 2 within the interval [RANGE_START, RANGE_END).
-"""
-powers_of_two = set()
 
 
 def is_power_of_two(x):
@@ -16,12 +12,29 @@ def is_power_of_two(x):
     return (x & (x - 1)) == 0
 
 
-def find_powers_of_two(iter):
-    for i in iter:
-        if is_power_of_two(i) == True:
-            powers_of_two.add(i)
-t1 = threading.Thread(target=find_powers_of_two,args=(range(RANGE_START,600),))
-t2 = threading.Thread(target=find_powers_of_two,args=(range(601,RANGE_END),))
-t1.start()
-t2.start()
+powers_of_two = set()
+set_lock = threading.Lock()
 
+
+def find_powers_of_two(iter):
+    for x in iter:
+        if is_power_of_two(x):
+            set_lock.acquire()
+            powers_of_two.add(x)
+            set_lock.release()
+
+
+thread1 = threading.Thread(target=find_powers_of_two, args=(range(RANGE_START, 250),))
+thread2 = threading.Thread(target=find_powers_of_two, args=(range(250, 500),))
+thread3 = threading.Thread(target=find_powers_of_two, args=(range(500, 750),))
+thread4 = threading.Thread(target=find_powers_of_two, args=(range(750, RANGE_END),))
+
+thread1.start()
+thread2.start()
+thread3.start()
+thread4.start()
+
+thread1.join()
+thread2.join()
+thread3.join()
+thread4.join()
